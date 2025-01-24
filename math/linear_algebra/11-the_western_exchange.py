50616b042516cc052079b5f5f12bc61e9c96c5cc
ngabo-dev <j.niyongabo@alustudent.com>
2025-01-24 06:15:42 -0800

    western exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for transposing a matrix.
+np_transpose function returns the transposed version of a given NumPy array.
+"""
+
+
+def np_transpose(matrix):
+    """Transposes a matrix
+    Args:
+        matrix: A numpy array to be transposed.
+    Returns:
+        A numpy array representing the transpose of the input matrix.
+    """
+    return matrix.transpose()
