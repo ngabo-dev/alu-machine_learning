55e398b2acba8d58226aeb476477519a5eaf9287
ngabo-dev <j.niyongabo@alustudent.com>
2025-01-24 05:18:26 -0800

    add matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 """
-Module to add two 2D matrices element-wise.
+add_matrices2D = __import__('5-across_the_planes').add_matrices2D
+
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add_matrices2D(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
 """
 
 
 def add_matrices2D(mat1, mat2):
-    """
-    Adds two 2D matrices element-wise.
+    """Adds two 2D matrices element-wise.
 
     Args:
-        mat1 (list of list of int/float): The first matrix.
-        mat2 (list of list of int/float): The second matrix.
+        mat1: A 2D list (matrix) of integers or floats.
+        mat2: A 2D list (matrix) of integers or floats.
 
     Returns:
-        list of list of int/float: A new matrix containing the element-wise sums.
-        None: If the matrices are not the same shape.
+        A new 2D list with the element-wise sum of mat1 and mat2.
+        If mat1 and mat2 are not the same shape, returns None.
     """
-    if len(mat1) != len(mat2) or any(
-        len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)
-    ):
+    if len(mat1) != len(mat2):
         return None
 
+    for row1, row2 in zip(mat1, mat2):
+        if len(row1) != len(row2):
+            return None
+
     return [
-        [mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
-        for i in range(len(mat1))
+        [row1[i] + row2[i] for i in range(len(row1))]
+        for row1, row2 in zip(mat1, mat2)
     ]
