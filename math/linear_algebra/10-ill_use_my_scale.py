c5799c1c6571066af078b0064e5f4707714a0e22
ngabo-dev <j.niyongabo@alustudent.com>
2025-01-24 06:08:41 -0800

    calculates the shape

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+"""
+Module for calculating the shape of a NumPy array.
+np_shape function calculates the shape of a NumPy array.
+"""
+
+
 def np_shape(matrix):
+    """Calculates the shape of a numpy.ndarray.
+    Args:
+        matrix: A numpy array whose shape needs to be calculated.
+    Returns:
+        A tuple of integers representing the shape of the numpy array.
+    """
     return tuple(matrix.shape)
