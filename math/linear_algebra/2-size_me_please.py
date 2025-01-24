c51c62a8c110ddfacf33714cf97586271b72461b
ngabo-dev <j.niyongabo@alustudent.com>
2025-01-24 04:13:11 -0800

    size me please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,9 +3,9 @@
 Module to calculate and print the shape of a matrix.
 """
 def matrix_shape(matrix):
-    """Calculates and prints the shape of a matrix."""
+    """Calculates the shape of a matrix."""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0]
-    print(shape)
+    return shape
