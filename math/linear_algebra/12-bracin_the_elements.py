99ecc50676c61c40adda6c2d661c50ab2446c120
ngabo-dev <j.niyongabo@alustudent.com>
2025-01-24 06:20:42 -0800

    bracing the elements

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that performs element-wise operations.
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise opreations.
+    Args:
+        mat1: A NumPy array.
+        mat2: A NumPy array or scalar.
+    Returns:
+        A tuple with element-wise sum, difference, product, and quotient.
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
