b0bd0df567d28446178b0e2ea1f59c6313bcbcae
ngabo-dev <j.niyongabo@alustudent.com>
2025-02-21 04:14:19 -0800

    normal and binomial

---
+++ math/probability/binomial.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Represents a binomial distribution.
+
+Can be initialized with given parameters or from a dataset. 
+Includes methods for PMF and CDF calculations.
+"""
+
+
+class Binomial:
+    """Represents a binomial distribution."""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """Initializes the binomial distribution."""
+        if data is None:
+            if n <= 0:
+                raise ValueError('n must be a positive value')
+            if not 0 < p < 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            mean = sum(data) / len(data)
+            var = sum((x - mean) ** 2 for x in data) / len(data)
+            self.p = 1 - (var / mean)
+            self.n = round(mean / self.p)
+            self.p = mean / self.n
+
+    @staticmethod
+    def factorial(k):
+        """Calculates factorial of a number."""
+        result = 1
+        for i in range(1, k + 1):
+            result *= i
+        return result
+
+    def pmf(self, k):
+        """Calculates the probability mass function."""
+        k = int(k)
+        if k < 0:
+            return 0
+        n_fact = self.factorial(self.n)
+        k_fact = self.factorial(k)
+        n_k_fact = self.factorial(self.n - k)
+        return (n_fact / (k_fact * n_k_fact)) *\
+            (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+    def cdf(self, k):
+        """Calculates the cumulative distribution function."""
+        k = int(k)
+        if k < 0:
+            return 0
+        return sum(self.pmf(i) for i in range(0, k + 1))
---
+++ math/probability/normal.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""Represents a normal distribution.
+
+Can be initialized with given parameters or from a dataset. 
+Includes methods for calculating z-scores, x-values, PDF, and CDF.
+"""
+
+
+class Normal:
+    """
+    A class that represents a normal distribution.
+    """
+
+    def __init__(self, data=None, mean=0.0, stddev=1.0):
+        """
+        Initialize the Normal distribution.
+        Args:
+            data
+            mean
+            stddev
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = (
+                sum((x - self.mean) ** 2 for x in data) / len(data)
+            ) ** 0.5
+
+    def z_score(self, x):
+        """Calculate the z-score of a given x-value."""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculate the x-value of a given z-score."""
+        return self.mean + z * self.stddev
+
+    def pdf(self, x):
+        """Calculate the Probability Density Function"""
+        coefficient = 1 / (self.stddev * (2 * self.pi) ** 0.5)
+        exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
+        return coefficient * self.e**exponent
+
+    def cdf(self, x):
+        """
+        Calculates the value of the
+        CDF for a given x-value
+        """
+        mean = self.mean
+        stddev = self.stddev
+        pi = 3.1415926536
+        value = (x - mean) / (stddev * (2 ** (1 / 2)))
+        val = value - ((value**3) / 3) + ((value**5) / 10)
+        val = val - ((value**7) / 42) + ((value**9) / 216)
+        val *= 2 / (pi ** (1 / 2))
+        cdf = (1 / 2) * (1 + val)
+        return cdf
+
+    @property
+    def pi(self):
+        """Approximation of pi."""
+        return 3.1415926535897932
+
+    @property
+    def e(self):
+        """Approximation of e."""
+        return 2.7182818284590452
