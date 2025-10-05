60ea53ec1b4f816b59ba92abf2be3a105ea475bf
Jean Pierre NIYONGABO <111361553+ngabo-dev@users.noreply.github.com>
2025-10-05 21:41:05 +0200

    Refactor Transformer class implementation
    Refactor Transformer class to inherit from tf.keras.Model and update docstrings.

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -1,28 +1,56 @@
 #!/usr/bin/env python3
-""" doc """
-
+""" Transformer Module """
 import tensorflow as tf
+
 Encoder = __import__('9-transformer_encoder').Encoder
 Decoder = __import__('10-transformer_decoder').Decoder
 
 
-class Transformer(tf.keras.layers.Layer):
-    """ doc """
-
+class Transformer(tf.keras.Model):
+    """ Transformer Network """
+    
     def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
                  max_seq_input, max_seq_target, drop_rate=0.1):
+        """
+        Initialize Transformer
+        
+        Args:
+            N: number of blocks in encoder and decoder
+            dm: dimensionality of the model
+            h: number of heads
+            hidden: number of hidden units in fully connected layer
+            input_vocab: size of input vocabulary
+            target_vocab: size of target vocabulary
+            max_seq_input: maximum sequence length for input
+            max_seq_target: maximum sequence length for target
+            drop_rate: dropout rate
+        """
         super(Transformer, self).__init__()
         self.encoder = Encoder(N, dm, h, hidden, input_vocab,
                                max_seq_input, drop_rate)
         self.decoder = Decoder(N, dm, h, hidden, target_vocab,
                                max_seq_target, drop_rate)
         self.linear = tf.keras.layers.Dense(target_vocab)
-
+    
     def call(self, inputs, target, training, encoder_mask,
              look_ahead_mask, decoder_mask):
-        """ doc """
+        """
+        Forward pass through Transformer
+        
+        Args:
+            inputs: input tensor
+            target: target tensor
+            training: boolean for training mode
+            encoder_mask: padding mask for encoder
+            look_ahead_mask: look ahead mask for decoder
+            decoder_mask: padding mask for decoder
+            
+        Returns:
+            final output tensor with shape (batch, target_seq_len, target_vocab)
+        """
         enc_output = self.encoder(inputs, training, encoder_mask)
         dec_output = self.decoder(target, enc_output, training,
                                   look_ahead_mask, decoder_mask)
-
-        return self.linear(dec_output)
+        final_output = self.linear(dec_output)
+        
+        return final_output
